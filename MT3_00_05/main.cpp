ed74903b5288babdc8f9ffaa2531d52ac8182c14
OkazakiTakuma <k024g0021@g.neec.ac.jp>
2025-04-18 11:50:17 +0900

    ℤ軸の回転の計算の修正

--- MT3_00_05/main.cpp
+++ MT3_00_05/main.cpp
@@ -12,7 +12,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 scale{1.2f, 0.79f, -2.1f};
 	Vector3 rotate{0.4f, 1.43f, -0.8f};
 	Vector3 translate{2.7f, -4.15f, 1.57f};
-	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
@@ -29,6 +28,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+		Matrix4x4 rotateMatrix = MakeRotateXYZMatrix(rotate);
+		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 		
 		///
 		/// ↑更新処理ここまで
@@ -39,7 +40,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		
 		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
-
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateMatrix, "rotateMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
